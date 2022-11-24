86b3a736310368c1ad1970dd1bb80c072f69baaf
pony <pony076152340@gmail.com>
2022-11-24 22:51:44 +0800

    dockerfile
    把一些dockerfile放上去

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shaker
+
+
+## 環境建置101
+
+### 基礎執行環境
+
+`docker-compose up -d`
+
+結束
---
+++ dockerfiles/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["/run/src/*"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
