427c47ecbc5bad29ff8749feddc07dc7d1ddd5ca
roy <roy05289113@gmail.com>
2022-12-03 21:20:13 +0800

    RRR VENV

--- Shaker/models.py
+++ Shaker/models.py
@@ -10,25 +10,26 @@ from django.db import models
 
 class Accounts(models.Model):
     account_id = models.CharField(max_length=255)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.CharField(max_length=255, blank=True, null=True)
-    password = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     last_login = models.DateTimeField(blank=True, null=True)
     photo = models.ForeignKey('Picture', models.DO_NOTHING, db_column='photo', blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'accounts'
 
 
 class Admin(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True)
-    account = models.CharField(max_length=255, blank=True, null=True)
-    password = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    account = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    publisher = models.ForeignKey('Publisher', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'admin'
 
 
@@ -38,114 +39,123 @@ class Cart(models.Model):
     count = models.IntegerField()
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'cart'
         unique_together = (('owner', 'product'),)
 
 
 class CategoryType(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'category_type'
 
 
 class Comment(models.Model):
     order = models.ForeignKey('Order', models.DO_NOTHING)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    star = models.IntegerField(blank=True, null=True)
+    product = models.ForeignKey('Product', models.DO_NOTHING)
+    star = models.IntegerField()
     description = models.CharField(max_length=255, blank=True, null=True)
-    picture = models.ForeignKey('Picture', models.DO_NOTHING, db_column='picture', blank=True, null=True)
-    time = models.DateTimeField(blank=True, null=True)
+    picture = models.ForeignKey('Picture', models.DO_NOTHING, db_column='picture')
+    time = models.DateTimeField()
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'comment'
 
 
 class Coupon(models.Model):
     id = models.IntegerField(primary_key=True)
-    owner = models.ForeignKey(Accounts, models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
+    publisher = models.ForeignKey('Publisher', models.DO_NOTHING)
+    name = models.CharField(max_length=255)
     type = models.ForeignKey('CouponType', models.DO_NOTHING, db_column='type')
     shop = models.ForeignKey('Shop', models.DO_NOTHING)
-    start_time = models.DateTimeField(blank=True, null=True)
+    start_time = models.DateTimeField()
     end_time = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'coupon'
 
 
 class CouponType(models.Model):
-    minimum_consumption = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
-    discount = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
-    dicount_type = models.IntegerField(blank=True, null=True)
+    minimum_consumption = models.DecimalField(max_digits=10, decimal_places=0)
+    discount = models.DecimalField(max_digits=10, decimal_places=0)
+    dicount_type = models.IntegerField()
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'coupon_type'
 
 
 class Order(models.Model):
-    owner_id = models.IntegerField(blank=True, null=True)
-    start_time = models.DateTimeField(blank=True, null=True)
-    end_time = models.DateTimeField(blank=True, null=True)
-    payment = models.IntegerField(blank=True, null=True)
-    status = models.IntegerField(blank=True, null=True)
-    free_fee = models.IntegerField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
+    owner_id = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    payment = models.IntegerField()
+    status = models.IntegerField()
+    free_fee = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'order'
 
 
 class OrderDetail(models.Model):
     order = models.ForeignKey(Order, models.DO_NOTHING)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    number = models.IntegerField(blank=True, null=True)
+    product = models.ForeignKey('Product', models.DO_NOTHING)
+    number = models.IntegerField()
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'order_detail'
 
 
 class Picture(models.Model):
     id = models.IntegerField(primary_key=True)
-    file_path = models.CharField(max_length=255, blank=True, null=True)
+    file_path = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'picture'
 
 
 class Product(models.Model):
-    shop = models.ForeignKey('Shop', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    number = models.IntegerField(blank=True, null=True)
-    intro = models.CharField(max_length=255, blank=True, null=True)
-    category = models.ForeignKey(CategoryType, models.DO_NOTHING, db_column='category', blank=True, null=True)
-    picture = models.ForeignKey(Picture, models.DO_NOTHING, blank=True, null=True)
+    shop = models.ForeignKey('Shop', models.DO_NOTHING)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    number = models.IntegerField()
+    intro = models.CharField(max_length=255)
+    category = models.ForeignKey(CategoryType, models.DO_NOTHING, db_column='category')
+    picture = models.ForeignKey(Picture, models.DO_NOTHING)
     avgstar = models.CharField(max_length=255, blank=True, null=True)
     status = models.IntegerField()
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'product'
 
 
-class Shop(models.Model):
-    owner = models.ForeignKey(Accounts, models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    avgstar = models.IntegerField(blank=True, null=True)
-    intro = models.CharField(max_length=255, blank=True, null=True)
-    last_login = models.DateTimeField(blank=True, null=True)
-    logo = models.ForeignKey(Picture, models.DO_NOTHING, db_column='logo', blank=True, null=True)
+class Publisher(models.Model):
+    publisher_id = models.AutoField(primary_key=True)
 
     class Meta:
-        managed = False
+        # managed = False
+        db_table = 'publisher'
+
+
+class Shop(models.Model):
+    owner = models.ForeignKey(Accounts, models.DO_NOTHING)
+    name = models.CharField(max_length=255)
+    avgstar = models.IntegerField()
+    intro = models.CharField(max_length=255, blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    logo = models.ForeignKey(Picture, models.DO_NOTHING, db_column='logo', null=True)
+    publisher = models.ForeignKey(Publisher, models.DO_NOTHING)
+
+    class Meta:
+        # managed = False
         db_table = 'shop'
