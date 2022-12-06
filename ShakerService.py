e51d3584edba10096ee1c7d2fc24fdc149d8f904
roy <roy05289113@gmail.com>
2022-12-06 22:56:41 +0800

    可以連結了

--- ShakerService.py
+++ ShakerService.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, make_response
-
-from configs import config
+from flask import Flask, render_template, make_response, Blueprint
+from apps import account_manage
+# from configs import config
 
 app = Flask(__name__)
+app.register_blueprint(account_manage.app, url_prefix='/account')
 # app.config.from_object()
 
 @app.route('/')
