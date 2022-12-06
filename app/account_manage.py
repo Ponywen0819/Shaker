500a55d5b3a3fc9b5fda3a754d9dd163c620e548
WeiXiangShen <e6031583@gmail.com>
2022-12-06 22:33:53 +0800

    換行

--- app/account_manage.py
+++ app/account_manage.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,request,redirect,url_for,blueprint
 import MySQLdb
+
 app = blueprint()
+
 @app.route("/check_duplicate", methods=['POST'])
 def check_duplicate():
     return 0
+
 @app.route("/register", methods=['POST'])
 def register():
     return 0
