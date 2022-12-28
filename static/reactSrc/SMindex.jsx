4860830492fcb03f323485360790202935c47aed
sacbem <sacbem7801@gmail.com>
2022-12-29 00:20:41 +0800

    fix: 修正 sellercenter 無法顯示之問題

--- static/reactSrc/SMindex.jsx
+++ static/reactSrc/SMindex.jsx
@@ -1,6 +1,8 @@
 const Main = ()=>{
     return [
         <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+
+            
             <Nav />
             <div className="mainContext flex-1">
                 <Sidebar />
